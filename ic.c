b9df1106231343e67f7f54d3e2206a78d39e87ae
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-02-02 16:15:04 +0530

    isisi.c

---
+++ ic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,o;
+printf("\n Upper limit ");
+scanf("%d",&a);
+printf("\n Lower limit ");
+scanf("%d",&b);
+printf("\nEven numbers bw ul and ll are");
+for(i=a;i<=b;++i)
+{
+o=i%2;
+if(o==0)
+printf("\n  %d",i);
+}
+return 0;
+}
